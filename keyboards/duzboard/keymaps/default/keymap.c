cb566dd8f194333cda606ba56aa2646afb09dd9b
Patrick <info@p-kant.de>
2021-11-13 18:12:45 +0100

    edited keymaps

--- keyboards/duzboard/keymaps/default/keymap.c
+++ keyboards/duzboard/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 enum layers{
   _BASE,
-  _NUM,
+  _STRG,
   _FN
 };
 
@@ -25,42 +25,42 @@ enum layers{
 
 /*
         base layer:
-        ,-----.,--------------------------------------------------------------------------------------.,-----------.
-        | ESC ||  ^  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  ß  |  ´  |  BkSp  || Del | ENC |
-        |-----||--------------------------------------------------------------------------------------||-----------|
-        | M1  || Tab   |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  Ü  |  +  |Enter || Pos1| End |
-        |-----||-------------------------------------------------------------------------------|      |`-----------'
+        ,-----.,--------------------------------------------------------------------------------------. ,-----------.
+        | ESC ||  ^  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  ß  |  ´  |  BkSp  | | Del | ENC |
+        |-----||--------------------------------------------------------------------------------------| |-----------|
+        | M1  || Tab   |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  Ü  |  +  |Enter | | Pos1| End |
+        |-----||-------------------------------------------------------------------------------|      | `-----------'
         | M2  || CapsLk |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Ö  |  Ä  |  #  |     |
-        |-----||--------------------------------------------------------------------------------------|,------.
-        | M3  ||LShift|  <  |  Y  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  -  |    RShift   ||  Up  |
-        |-----||--------------------------------------------------------------------------------------'|------|------.
-        | FN  || LCtl | Win  | LAlt |                 Space              | AltGr | Menu | RCtl || Left |  Dn  | Rght |
-        `-----'`-------------------------------------------------------------------------------'`--------------------'
+        |-----||--------------------------------------------------------------------------------------| ,------.
+        | M3  ||LShift|  <  |  Y  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  -  |    RShift   | |  Up  |
+        |-----||--------------------------------------------------------------------------------------'-|------|------.
+        | FN  || LCtl | Win  | LAlt |                 Space              | AltGr | Menu | RCtl | | Left |  Dn  | Rght |
+        `-----'`-------------------------------------------------------------------------------' `--------------------'
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_all(
-        KC_ESC, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,  KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,  
+        KC_ESC, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,  KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
         KC_F1, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_HOME, KC_END,
-        KC_F2, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,   
-        KC_F3, KC_LSFT, KC_NUBS, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,  
+        KC_F2, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,
+        KC_F3, KC_LSFT, KC_NUBS, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
         KC_F4, KC_LCTL, KC_LGUI, KC_LALT,   KC_SPACE,    KC_ALGR, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
         ),
 
-    [_NUM] = LAYOUT_all(
-        KC_ESC, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,  KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, 
-        KC_F1, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_HOME, KC_END,
-        KC_F2, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,   
-        KC_F3, KC_LSFT, KC_NUBS, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,  
-        KC_F4, KC_LCTL, KC_LGUI, KC_LALT,   KC_SPACE,    KC_ALGR, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    [_STRG] = LAYOUT_all(
+        KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,  KC_0, KC_MINS, KC_EQL, KC_TRNS, KC_TRNS,
+        KC_MUTE, KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_VOLU, KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,
+        KC_VOLD, KC_TRNS, KC_NUBS, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS, KC_TRNS,
+        KC_F4, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         ),
-     
+
     [_FN] = LAYOUT_all(
-        KC_ESC, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,  KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, 
-        KC_F1, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_HOME, KC_END,
-        KC_F2, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,  
-        KC_F3, KC_LSFT, KC_NUBS, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,  
-        KC_F4, KC_LCTL, KC_LGUI, KC_LALT,   KC_SPACE,    KC_ALGR, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS,
+        KC_F1, KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_TRNS, KC_PSCR, KC_TRNS,
+        KC_F2, KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,
+        KC_F3, KC_TRNS, KC_NUBS, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS, KC_TRNS,
+        KC_F4, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         )
 };
